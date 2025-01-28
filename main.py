f0292142edb26a44afd702bcd3ec195f69aaae74
carnasci <cen.master24@gmail.com>
2025-01-28 16:46:22 -0600

    Day 29 - touched up UI

--- main.py
+++ main.py
@@ -20,12 +20,14 @@ website_label.grid(row=1, column=0)
 
 website_input = Entry(width=52)
 website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
 
 username_label = Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
 
 username_input = Entry(width=52)
 username_input.grid(row=2, column=1, columnspan=2)
+username_input.insert(0, "email@email.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
