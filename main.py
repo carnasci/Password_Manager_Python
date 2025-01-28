c9405e54e9b1eaf86e09a16f4f7c0ab24cb1b7e8
carnasci <cen.master24@gmail.com>
2025-01-28 16:39:03 -0600

    Day 29 - completed UI

--- main.py
+++ main.py
@@ -8,12 +8,36 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200, width=200, highlightthickness=0)
+canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_input = Entry(width=52)
+website_input.grid(row=1, column=1, columnspan=2)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(row=2, column=0)
+
+username_input = Entry(width=52)
+username_input.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_input = Entry(width=34)
+password_input.grid(row=3, column=1)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 
