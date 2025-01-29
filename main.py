d6b8dcabdac3b26e4be413d45eca0f5be1397481
carnasci <cen.master24@gmail.com>
2025-01-28 18:16:45 -0600

    Day 29 - added autocopy password to clipboard when generated, day 29 finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -19,6 +20,7 @@ def generate_password():
 
     password = "".join(password_list)
     password_input.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
